8a0dd350b8ef2ce8ddbd3c20112f4c9de8413ea6
Muhammad Fadhlan Harits <fadhlan.harits@gmail.com>
2021-09-23 09:24:04 +0700

    nambahin folder, nanti bikin kodingan di dalem folder aja

--- search-queue/search.py
+++ search-queue/search.py
@@ -13,7 +13,7 @@ numbers = [4, 5, 78, 2, 15]
 num5 = search(numbers, 5)
 print('Angka 5 ada pada indeks ' + str(num5))
 '''
-
+# Nyari kelas
 def search(arr, x):
   
     for i in range(len(arr)):
@@ -24,20 +24,44 @@ def search(arr, x):
   
     return -1, None
 
-mul_arr = [
+# Masukin data 
+def insert(arr, x):
+  
+    for i in range(len(arr)):
+        for j in range(len(arr[i])):
+  
+            if arr[i][j] == None:
+                arr[i][j] = x
+  
+    return arr
+
+
+jadwal = [
     #                       Jam 
     #  8            10           13              16
     ['IoT',     'Prak Jartel',  None,        'Big Data'],    # Senin
     ['Jartel',      None,       None,            None],       # Selasa
     [None,          None,       None,           None],         # Rabu
-    ['Bimbingan',   None,       'Dasreg',        15],          # Kamis
+    ['Bimbingan',   None,       'Dasreg',        'IoT'],          # Kamis
     ['Bimbingan',   None,       None,          'Despro']       # Jumat
 ]
 
-indeks1, indeks2 = search(mul_arr, 'Big Data')
+print(' ')
+print('Original array')
+print(jadwal)
 
-indeks_a, indeks_b = search(mul_arr, 'Despro')
-print('Kelas big data ada pada indeks ' + str(indeks1) + ' ' + str(indeks2))
-print('Kelas Despro ada pada indeks ' + str(indeks_a) + ' ' + str(indeks_b))
+recommend = insert(jadwal, 'Tugas')
+
+print(' ')
+print('Modified array')
+print(recommend)
+
+
+#indeks1, indeks2 = search(mul_arr, 'Big Data')
+
+#indeks_a, indeks_b = search(mul_arr, 'Despro')
+# print('Kelas big data ada pada indeks ' + str(indeks1) + ' ' + str(indeks2))
+# print('Kelas Despro ada pada indeks ' + str(indeks_a) + ' ' + str(indeks_b))
+
 
 
