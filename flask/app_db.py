77666608a84f9f496d1c909207409f8cbd449f52
Muhammad Fadhlan Harits <fadhlan.harits@gmail.com>
2021-11-07 15:10:28 +0700

    tambah rekomendasi jadwal

--- flask/app_db.py
+++ flask/app_db.py
@@ -1,4 +1,4 @@
-from flask import Flask, g, render_template, url_for, request, redirect
+from flask import Flask, g, render_template, url_for, request, redirect, jsonify
 import sqlite3
 
 app = Flask(__name__)
@@ -21,7 +21,6 @@ def index():
         kode_matkul = db.fetchone()[0]
 
         try:
-            
             db.execute("INSERT INTO mycourse(course_id) VALUES(?)", [kode_matkul])
             return redirect('/')
         except:
@@ -30,6 +29,15 @@ def index():
     else:
         course = db.execute("SELECT course.id AS course_id, course.name AS name, course.sks AS sks, course.term AS term, time(pertemuan.jam_start) AS jam_start, time(pertemuan.jam_end) AS jam_end, harienum.hari AS hari FROM course INNER JOIN pertemuan ON pertemuan.course_id = course.id INNER JOIN harienum ON harienum.id = pertemuan.hari GROUP BY harienum.hari ORDER BY term").fetchall()
         mycourse = db.execute("SELECT mycourse.id, mycourse.course_id AS course_id, course.name AS name, course.sks AS sks, course.term AS term, pertemuan.jam_start AS jam_start, pertemuan.jam_end AS jam_end, harienum.hari AS hari FROM mycourse INNER JOIN course ON course.id = mycourse.course_id INNER JOIN pertemuan ON pertemuan.course_id = course.id INNER JOIN harienum ON harienum.id = pertemuan.hari ORDER BY term").fetchall()
+        
+        #for crs in course:
+           # all_course = [{'id':crs[0],'name':crs[1],'sks':crs[2],'term':crs[3],'jam_start':crs[4],'jam_end':crs[5],'hari':crs[6]} for crs in course]
+        
+        # for mycrs in mycourse: 
+           # all_mycrs = [{'id':mycrs[0],'course_id':mycrs[1],'course_name':mycrs[2],'sks':mycrs[3],'jam_start':crs[4],'jam_end':crs[5],'hari':crs[6]} for crs in course]
+        
+
+        #return jsonify(all_course)
         return render_template('index.html', course=course, mycourse=mycourse)
 
 
@@ -37,20 +45,51 @@ def index():
 def submit():
         mycourse = db.execute("SELECT mycourse.id, mycourse.course_id AS course_id, course.name AS name, course.sks AS sks, course.term AS term, time(pertemuan.jam_start) AS jam_start, time(pertemuan.jam_end) AS jam_end, harienum.hari AS hari FROM mycourse INNER JOIN course ON course.id = mycourse.course_id INNER JOIN pertemuan ON pertemuan.course_id = course.id INNER JOIN harienum ON harienum.id = pertemuan.hari ORDER BY term").fetchall()
         
-        sched = []
-        for j in mycourse:
-            sched.append(j[2])
-
-        return render_template('submit.html', mycourse=mycourse, sched=sched)
+        return render_template('submit.html', mycourse=mycourse)
 
 
 @app.route('/generate', methods=['POST', 'GET'])
 def generate():
         mycourse = db.execute("SELECT mycourse.id, mycourse.course_id AS course_id, course.name AS name, course.sks AS sks, course.term AS term, time(pertemuan.jam_start) AS jam_start, time(pertemuan.jam_end) AS jam_end, harienum.hari AS hari FROM mycourse INNER JOIN course ON course.id = mycourse.course_id INNER JOIN pertemuan ON pertemuan.course_id = course.id INNER JOIN harienum ON harienum.id = pertemuan.hari ORDER BY term").fetchall()
         
-        sched = []
+        sched = [] # array isinya nama matkul yang dipilih
+        day = []
+        rec = [] # array isinya rekomendasi jadwal 
         for j in mycourse:
-            sched.append(j[2])
+            sched.append(j[2]) # array diisi nama matkul
+            day.append(j[7])
+        
+        # sched udah keisi 
+        # [jarkom, kemjar, big data]
+        
+        for i in sched:
+            # jarkom - selasa & kamis
+            if i == "Jaringan Komputer":
+                # rekomendasi jarkom selasa malem
+                rec.append("Rec Jarkom Selasa")
+                
+                # kalo ga ngambil kemjar
+                if 'Keamanan Jaringan' not in sched:
+                    # rekomendasi jarkom rabu sore
+                    rec.append("Rec Jarkom Rabu")
+                    
+                else: 
+                    # rekomendasi jarkom rabu pagi 
+                    rec.append("Rec Jarkom Rabu Pagi")
+
+            elif i == "Keamanan Jaringan":
+                rec.append("Rec Kemjar Rabu")
+
+            elif i == "Teknologi Big Data":
+                rec.append("Rec Big Data")
+
+            
+            elif i == "Sistem Waktu Nyata dan IoT":
+                rec.append("Rec IoT Kamis")
+
+            elif i == "Despro":
+                rec.append("Rec Despro") 
+
 
         db.execute("SELECT MAX(id) FROM mycourse")
         start = db.fetchone()[0]
@@ -62,7 +101,7 @@ def generate():
         
         alokasi = total_sks * 120
 
-        return render_template('submit.html', mycourse=mycourse, total_sks=total_sks, alokasi=alokasi, sched=sched)
+        return render_template('generate.html', mycourse=mycourse, total_sks=total_sks, alokasi=alokasi, sched=sched, rec=rec)
     
 
 if __name__ == '__main__':
