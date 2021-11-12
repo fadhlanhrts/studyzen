d62cbade642891025d25874241d97b82c862c048
idhamramadito <idhamadito@gmail.com>
2021-11-12 15:16:32 +0700

    buat page post

--- flutter/lib/post.dart
+++ flutter/lib/post.dart
@@ -1,74 +1,18 @@
-import 'dart:async';
-import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'dart:convert';
 import 'package:http/http.dart' as http;
-
-Future<Album> createAlbum(String title) async {
-  final response = await http.post(
-    Uri.parse('http://96f9-118-136-163-170.ngrok.io/'),
-    headers: <String, String>{
-      'Content-Type': 'application/json; charset=UTF-8',
-    },
-    body: jsonEncode(<String, String>{
-      'title': title,
-    }),
-  );
-
-  if (response.statusCode == 201) {
-    // If the server did return a 201 CREATED response,
-    // then parse the JSON.
-    return Album.fromJson(jsonDecode(response.body));
-  } else {
-    // If the server did not return a 201 CREATED response,
-    // then throw an exception.
-    throw Exception('Failed to create album.');
-  }
-}
-
-class Album {
-  final String hari;
-  final int id;
-  final String jam_end;
-  final String jam_start;
-  final String name;
-  final int sks;
-  final int term;
-
-
-  Album({
-         required this.hari,
-         required this.id,
-         required this.jam_end,
-         required this.jam_start,
-         required this.name,
-         required this.sks,
-         required this.term});
-
-  factory Album.fromJson(Map<String, dynamic> json) {
-    return Album(
-      hari: json['hari'],
-      id: json['id'],
-      jam_end: json['jam_end'],
-      jam_start: json['jam_start'],
-      name: json['name'],
-      sks: json['sks'],
-      term: json['term']
-    );
-  }
-}
-
 
 class PostPage extends StatefulWidget {
 
   @override
-  _PostPageState createState() {
-    return _PostPageState();
-  }
+  _PostPageState createState() => _PostPageState();
 }
 
 class _PostPageState extends State<PostPage> {
-  final TextEditingController _controller = TextEditingController();
-  Future<Album>? _futureAlbum;
+
+  TextEditingController postNum = TextEditingController();
+
+  bool pressed = false;
 
   @override
   Widget build(BuildContext context) {
@@ -79,49 +23,116 @@ class _PostPageState extends State<PostPage> {
       ),
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('POST DATA'),
+          title: Text('POST DATA'),
         ),
-        body: Container(
-          alignment: Alignment.center,
-          padding: const EdgeInsets.all(8.0),
-          child: (_futureAlbum == null) ? buildColumn() : buildFutureBuilder(),
+        body: Column(
+          children: <Widget>[
+            TextField(
+              controller: postNum,
+              decoration: InputDecoration(
+                hintText: "Enter Post Id",
+                // border: InputBorder.none,
+              ),
+
+            ),
+            RaisedButton(child: Text("Post"),
+                onPressed: () => {setState(() {
+               pressed = true;
+              //fetchData(postNum);
+            })}),
+
+            pressed ? fetchData(postNum): SizedBox(),
+
+          ],
         ),
       ),
     );
   }
+}
 
-  Column buildColumn() {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
+class Posts {
+  final String hari;
+  // final int id;
+  final String jam_end;
+  final String jam_start;
+  final String name;
+  // final int sks;
+  // final int term;
+
+  Posts({required this.hari,
+        //  required this.id,
+         required this.jam_end,
+         required this.jam_start,
+         required this.name,
+        //  required this.sks,
+        //  required this.term
+         });
+
+  factory Posts.fromJson(Map<String, dynamic> json) {
+    return Posts(
+      hari: json['hari'],
+      // id: json['id'],
+      jam_end: json['jam_end'],
+      jam_start: json['jam_start'],
+      name: json['name'],
+      // sks: json['sks'],
+      // term: json['term']
+    );
+  }
+}
+
+Future<Posts> fetchPosts(String name) async {
+  final response =
+  await http.get(Uri.parse('http://96f9-118-136-163-170.ngrok.io/'));
+
+  if (response.statusCode == 200) {
+    // If the server did return a 200 OK response,
+    // then parse the JSON.
+    return Posts.fromJson(json.decode(response.body)[0]);
+  } else {
+    // If the server did not return a 200 OK response,
+    // then throw an exception.
+    throw Exception('Failed to load album');
+  }
+}
+
+FutureBuilder<Posts> fetchData(postNum) {
+  return FutureBuilder<Posts>(
+    future: fetchPosts(postNum.text.toString()),
+    builder: (context, snapshot) {
+      if (snapshot.hasData) {
+        return getData(snapshot);
+      } else if (snapshot.hasError) {
+        return Text("${snapshot.error}");
+      }
+
+      // By default, show a loading spinner.
+      return CircularProgressIndicator();
+    },
+  );
+}
+
+Widget getData(snapshot) {
+  return Padding(
+    padding: const EdgeInsets.all(35.0),
+    child: Column(
       children: <Widget>[
-        TextField(
-          controller: _controller,
-          decoration: const InputDecoration(hintText: 'Enter POST'),
-        ),
-        ElevatedButton(
-          onPressed: () {
-            setState(() {
-              _futureAlbum = createAlbum(_controller.text);
-            });
-          },
-          child: const Text('POST DATA'),
-        ),
+
+        Padding(padding: EdgeInsets.all(5)),
+        Text("Hari : " + snapshot.data.hari, style: TextStyle(fontSize: 20)),
+        // Padding(padding: EdgeInsets.all(20)),
+        // Text("Id : " + snapshot.data.id, style: TextStyle(fontSize: 20)),
+        Padding(padding: EdgeInsets.all(5)),
+        Text("Jam End : " + snapshot.data.jam_end, style: TextStyle(fontSize: 20)),
+        Padding(padding: EdgeInsets.all(5)),
+        Text("Jam Start : " + snapshot.data.jam_start, style: TextStyle(fontSize: 20)),
+        Padding(padding: EdgeInsets.all(5)),
+        Text("Name : " + snapshot.data.name, style: TextStyle(fontSize: 20)),
+        // Padding(padding: EdgeInsets.all(20)),
+        // Text("SKS : " + snapshot.data.sks, style: TextStyle(fontSize: 20)),
+        // Padding(padding: EdgeInsets.all(20)),
+        // Text("Term : " + snapshot.data.term, style: TextStyle(fontSize: 20)),
       ],
-    );
-  }
-
-  FutureBuilder<Album> buildFutureBuilder() {
-    return FutureBuilder<Album>(
-      future: _futureAlbum,
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          return Text(snapshot.data!.hari);
-        } else if (snapshot.hasError) {
-          return Text('${snapshot.error}');
-        }
-
-        return const CircularProgressIndicator();
-      },
-    );
-  }
+    ),
+  );
 }
