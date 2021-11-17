24725010ca6c86db8e7d9ed264a979410342b887
idhamramadito <idhamadito@gmail.com>
2021-11-17 19:06:32 +0700

    bersiin kodingan

--- flutter/lib/table.dart
+++ flutter/lib/table.dart
@@ -1,60 +1,58 @@
-import 'package:flutter/material.dart';  
+import 'package:flutter/material.dart';
 
-  
-class TablePage extends StatelessWidget {  
-  @override  
-  Widget build(BuildContext context) {  
-    return MaterialApp(  
-      home: Scaffold(  
-          appBar: AppBar(  
-            title: Text('Table'),  
-          ),  
-          body: ListView(children: <Widget>[  
-            Center(  
-                child: Text(  
-                  'People-Chart',  
-                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),  
-                )),  
-            DataTable(  
-              columns: [  
-                DataColumn(label: Text(  
-                    'ID',  
-                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)  
-                )),  
-                DataColumn(label: Text(  
-                    'Name',  
-                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)  
-                )),  
-                DataColumn(label: Text(  
-                    'Profession',  
-                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)  
-                )),  
-              ],  
-              rows: [  
-                DataRow(cells: [  
-                  DataCell(Text('1')),  
-                  DataCell(Text('Stephen')),  
-                  DataCell(Text('Actor')),  
-                ]),  
-                DataRow(cells: [  
-                  DataCell(Text('5')),  
-                  DataCell(Text('John')),  
-                  DataCell(Text('Student')),  
-                ]),  
-                DataRow(cells: [  
-                  DataCell(Text('10')),  
-                  DataCell(Text('Harry')),  
-                  DataCell(Text('Leader')),  
-                ]),  
-                DataRow(cells: [  
-                  DataCell(Text('15')),  
-                  DataCell(Text('Peter')),  
-                  DataCell(Text('Scientist')),  
-                ]),  
-              ],  
-            ),  
-          ])  
-      ),  
-    );  
-  }  
-}  
+class TablePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text('Table'),
+          ),
+          body: ListView(children: <Widget>[
+            Center(
+                child: Text(
+              'People-Chart',
+              style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+            )),
+            DataTable(
+              columns: [
+                DataColumn(
+                    label: Text('ID',
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold))),
+                DataColumn(
+                    label: Text('Name',
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold))),
+                DataColumn(
+                    label: Text('Profession',
+                        style: TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold))),
+              ],
+              rows: [
+                DataRow(cells: [
+                  DataCell(Text('1')),
+                  DataCell(Text('Stephen')),
+                  DataCell(Text('Actor')),
+                ]),
+                DataRow(cells: [
+                  DataCell(Text('5')),
+                  DataCell(Text('John')),
+                  DataCell(Text('Student')),
+                ]),
+                DataRow(cells: [
+                  DataCell(Text('10')),
+                  DataCell(Text('Harry')),
+                  DataCell(Text('Leader')),
+                ]),
+                DataRow(cells: [
+                  DataCell(Text('15')),
+                  DataCell(Text('Peter')),
+                  DataCell(Text('Scientist')),
+                ]),
+              ],
+            ),
+          ])),
+    );
+  }
+}
